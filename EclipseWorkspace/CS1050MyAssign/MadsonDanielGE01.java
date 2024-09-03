264ed40d9bb85f3a3ccb0be823052ce1dd073fcd
dmads2 <dmadson2203@gmail.com>
2024-09-02 21:31:47 -0600

    Update MadsonDanielGE01.java

--- EclipseWorkspace/CS1050MyAssign/MadsonDanielGE01.java
+++ EclipseWorkspace/CS1050MyAssign/MadsonDanielGE01.java
@@ -4,7 +4,7 @@
  * T/R Section
  * 9/2/2024
  * GE01
- * This program will calculate grades.
+ * This program will calculate grades and also explain a little bit about myself.
  * 
  */
 
@@ -18,6 +18,7 @@ public class MadsonDanielGE01 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//two paragraphs about myself
 		System.out.println("Hello my name is Daniel Madson. I've been passionate about computers ever since I first started playing videogames.");
 		System.out.println("This has led me to pursue a computer science degree. Although I am not a huge fan, I think I will stick it out anyway,");
 		System.out.println("as I feel like it is a very valuable degree and I don't necessarily need to go into software development with it.");
@@ -26,6 +27,29 @@ public class MadsonDanielGE01 {
 		System.out.println("At the moment, I would love to finish college as soon as possible, so I am planning on taking winter and summer classes.");
 		System.out.println("Generally speaking, programming is not my passion nor do I find it very interesting, so I am thinking about taking a");
 		System.out.println("minor in business or maybe finance, as that is what I really enjoy- as long as it does not push back my graduation.");
+		System.out.println();
+		
+		//creating constants for grade weights
+		final double GE_WEIGHT = 0.15;
+		final double PART_WEIGHT = 0.10;
+		final double COMM_WEIGHT = 0.10;
+		final double QUIZ_WEIGHT = 0.10;
+		final double PROJECT_WEIGHT = 0.20;
+		final double MIDTERM_WEIGHT = 0.15;
+		final double FINALEXAM_WEIGHT = 0.20;
+		
+		//declaring my assignment grades for future calculation
+		double gegrade = 95.0;
+		double participationgrade = 92.0;
+		double commgrade = 88.0;
+		double quizgrade = 87.0;
+		double projectgrade = 93.0;
+		double midtermgrade = 70.0;
+		double finalexamgrade = 85.0;
+		
+		
+		
+
 	}
 
 }
