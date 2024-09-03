8f4bb50dbe5f837517a4b7823cfa025930bdc385
dmads2 <dmadson2203@gmail.com>
2024-09-02 21:14:59 -0600

    Update MadsonDanielGE01.java

--- EclipseWorkspace/CS1050MyAssign/MadsonDanielGE01.java
+++ EclipseWorkspace/CS1050MyAssign/MadsonDanielGE01.java
@@ -1,8 +1,14 @@
-/**
+/*
+ * Daniel Madson
+ * CS 1050
+ * T/R Section
+ * 9/2/2024
+ * GE01
+ * This program will calculate grades.
  * 
  */
 
-/**
+/*
  * 
  */
 public class MadsonDanielGE01 {
@@ -12,7 +18,14 @@ public class MadsonDanielGE01 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello my name is Daniel Madson. I've been passionate about computers ever since I first started playing videogames.");
+		System.out.println("This has led me to pursue a computer science degree. Although I am not a huge fan, I think I will stick it out anyway,");
+		System.out.println("as I feel like it is a very valuable degree and I don't necessarily need to go into software development with it.");
+		System.out.println("Perhaps I will go into a different tech role, or maybe even a completely different industry.");
+		System.out.println();
+		System.out.println("At the moment, I would love to finish college as soon as possible, so I am planning on taking winter and summer classes.");
+		System.out.println("Generally speaking, programming is not my passion nor do I find it very interesting, so I am thinking about taking a");
+		System.out.println("minor in business or maybe finance, as that is what I really enjoy- as long as it does not push back my graduation.");
 	}
 
 }
