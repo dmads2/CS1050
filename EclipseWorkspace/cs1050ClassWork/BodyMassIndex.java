3884c5d5103deed768b688cd429ae1ecff531606
dmads2 <dmadson2203@gmail.com>
2024-09-03 19:37:13 -0600

    Update BodyMassIndex.java

--- EclipseWorkspace/cs1050ClassWork/BodyMassIndex.java
+++ EclipseWorkspace/cs1050ClassWork/BodyMassIndex.java
@@ -4,6 +4,7 @@
  */
 
 // ADD CODE to import Scanner class 
+import java.util.Scanner;
 
 public class BodyMassIndex
 {
@@ -16,25 +17,30 @@ public class BodyMassIndex
 		double height; // The user's height
 		double bmi; // The user's BMI
 
-		// Create a Scanner object for scanning  input from keyboard 
+		// Create a Scanner object for scanning input from keyboard 
+		Scanner input = new Scanner(System.in);
+		
 		// ADD CODE name object variable input 
-
+		
+		
 		// Tell the user what the program will do.
 		System.out.println("This program will calculate your " + "body mass index, or BMI.");
 
 		// Get the user's weight.
 		System.out.print("Enter your weight, in pounds: ");
 		// ADD CODE to input next double for weight
+		weight = input.nextDouble();
 
 		// Get the user's height.
 		System.out.print("Enter your height, in inches: ");
 		// ADD CODE input next double for height
+		height = input.nextDouble();
 
 		// Calculate the user's body mass index.
 		bmi = weight * BMI_US_FACTOR / (height * height);
 
 		// Display the user's BMI.
-		System.out.printf("Your body mass index (BMI) is %f", bmi);
+		System.out.printf("Your body mass index (BMI) is %.1f", bmi);
 
 
 	}
