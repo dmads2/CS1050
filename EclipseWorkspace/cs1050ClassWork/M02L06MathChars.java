9a3cd8f5c5dff19afa547af07022078e54215fb4
dmads2 <dmadson2203@gmail.com>
2024-09-05 19:46:40 -0600

    Create M02L06MathChars.java

---
+++ EclipseWorkspace/cs1050ClassWork/M02L06MathChars.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class M02L06MathChars {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
