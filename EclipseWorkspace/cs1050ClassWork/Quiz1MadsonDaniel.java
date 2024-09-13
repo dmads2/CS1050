ff83a59d142a2c2b095db4de12e44bc0858d4e5f
dmads2 <dmadson2203@gmail.com>
2024-09-12 19:31:04 -0600

    Update Quiz1MadsonDaniel.java

--- EclipseWorkspace/cs1050ClassWork/Quiz1MadsonDaniel.java
+++ EclipseWorkspace/cs1050ClassWork/Quiz1MadsonDaniel.java
@@ -12,6 +12,15 @@ public class Quiz1MadsonDaniel {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//variables and constants
+		final double PI = 3.14;
+		double radius = 5.5;
+		
+		//half circle formula
+		double area = 0.5 * PI * radius * radius;
+
+		System.out.printf("The area of the half circle is: "+ "%.2f", area);
 
 	}
 
