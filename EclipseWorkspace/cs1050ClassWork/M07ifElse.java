b83212a8b36c8cc3f3ec32c1a07db416a89e8284
dmads2 <dmadson2203@gmail.com>
2024-09-23 12:20:59 -0600

    Update M07ifElse.java

--- EclipseWorkspace/cs1050ClassWork/M07ifElse.java
+++ EclipseWorkspace/cs1050ClassWork/M07ifElse.java
@@ -14,13 +14,31 @@ public class M07ifElse {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter a character.");
-		Scanner input = new Scanner(System.in);
-		String aString = input.next();
-		input.close();
-		char someChar = aString.charAt(0);
-		System.out.println("Character entered is " + someChar);
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a character.");
+		char someChar = input.next().charAt(0);
+
+		if(Character.isDigit(someChar))
+		{
+			System.out.println("its a number");
+		}
+		
+		if(someChar == '5')
+		{
+			System.out.printf("chars are the same");
+		}
+		
+		input.close();
+		
+		String str1 = "I love Java";
+		String str2 = "I LOVE JAVA";
+		
+		if (str1.equals(str2))
+		{ 
+			System.out.println("str1 and str2 are the same");
+		}
 		
 	}
 
