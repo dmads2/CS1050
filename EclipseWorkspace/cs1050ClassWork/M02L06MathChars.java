529340409ee194848e9be718fa496a592958a67a
dmads2 <dmadson2203@gmail.com>
2024-09-23 20:31:10 -0600

    Update M02L06MathChars.java

--- EclipseWorkspace/cs1050ClassWork/M02L06MathChars.java
+++ EclipseWorkspace/cs1050ClassWork/M02L06MathChars.java
@@ -10,11 +10,12 @@ public class M02L06MathChars {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 		
-		random():double - Math
+		
 	}
 
 }
