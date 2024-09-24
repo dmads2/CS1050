87b3ceef65b056153d24226853064eeb67f50dab
dmads2 <dmadson2203@gmail.com>
2024-09-23 23:16:41 -0600

    Update MadsonFirstJavaProgram.java

--- EclipseWorkspace/cs1050ClassWork/MadsonFirstJavaProgram.java
+++ EclipseWorkspace/cs1050ClassWork/MadsonFirstJavaProgram.java
@@ -5,6 +5,8 @@
 /**
  * 
  */
+import java.util.Scanner;
+
 public class MadsonFirstJavaProgram {
 
 	/**
@@ -12,8 +14,59 @@ public class MadsonFirstJavaProgram {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
-
+		/*
+		 * System.out.print("Hello");
+		
+		Scanner input = new Scanner(System.in);
+		
+		double newvariable = input.nextDouble();
+		
+		char newChar = input.next().charAt(0);
+		
+		String studentName = "danny madson";
+		
+		//char newChar = 'a';
+		
+		String newString = input.next();
+		
+		input.close();
+		*/
+		
+		//example of || and &&
+		
+		/*int a = 5;
+		int b = 10;
+		
+		if ((a > b) && (a > 100))
+		{
+			System.out.println("this is just an example");
+		}
+		
+		*/
+		
+		//nested if example
+		
+		/*
+		
+		int numOfStudents = 30;
+		String studentType = "MATH_STUDENT";
+		String MATH_STUDENT = "30";
+		
+		if (numOfStudents <= 30)
+		{
+			System.out.println("the class has space for you to register");
+			
+			if(studentType == MATH_STUDENT)
+			{
+				System.out.println("we can place you in the math class");
+			}
+		else
+		{
+			System.out.println("there is no space left in the class");
+		}
+		}
+		
+		*/
 	}
 
 }
