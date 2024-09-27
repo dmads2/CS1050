2ba50f25766eb5a15b1c2812cb6c13781476dd6c
dmads2 <dmadson2203@gmail.com>
2024-09-26 18:55:39 -0600

    Create MadsonDanielM02Quiz.java

---
+++ EclipseWorkspace/cs1050ClassWork/MadsonDanielM02Quiz.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class MadsonDanielM02Quiz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
