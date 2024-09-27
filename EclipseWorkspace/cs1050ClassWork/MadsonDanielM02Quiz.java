a527bbc06dc8841c536d08d0e0c026f9e1aff1ef
dmads2 <dmadson2203@gmail.com>
2024-09-26 19:15:52 -0600

    Update MadsonDanielM02Quiz.java

--- EclipseWorkspace/cs1050ClassWork/MadsonDanielM02Quiz.java
+++ EclipseWorkspace/cs1050ClassWork/MadsonDanielM02Quiz.java
@@ -5,16 +5,51 @@
 /**
  * 
  */
-public class MadsonDanielM02Quiz {
+import java.util.Scanner;
+
+public class MadsonDanielM02Quiz 
+{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		
+		Scanner input = new Scanner(System.in);
 		
-
+		System.out.println("Enter a grade from 0 to 100.");
+		double grade = input.nextDouble();
+		
+		if ((grade >= 0) && (grade <= 100))
+		{
+			if (grade >= 90)
+			{
+				System.out.println("Your letter grade is A.");
+			}
+			else if ((grade < 90) && (grade >= 80))
+			{
+				System.out.println("Your letter greade is B.");
+			}
+			else if ((grade < 80) && (grade >= 70))
+			{
+				System.out.println("Your letter grade is C.");
+			}
+			else if ((grade < 70) && (grade >= 60))
+			{
+				System.out.println("Your letter grade is D.");
+			}
+			else if (grade < 60)
+			{
+				System.out.println("Your letter grade is F.");
+			}
+		}
+		else 
+		{
+			System.out.println("The grade you entered is invalid.");
+		}
+		
+		input.close();
 	}
-
 }
+
