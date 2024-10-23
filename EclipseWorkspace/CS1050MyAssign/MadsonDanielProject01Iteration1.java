27db08454a6b3f5bbbdefe7cc9cff2f1f20bb136
dmads2 <dmadson2203@gmail.com>
2024-10-23 00:27:54 -0600

    working on project 1

--- EclipseWorkspace/CS1050MyAssign/MadsonDanielProject01Iteration1.java
+++ EclipseWorkspace/CS1050MyAssign/MadsonDanielProject01Iteration1.java
@@ -5,6 +5,7 @@
 /**
  * 
  */
+import java.util.Scanner;
 public class MadsonDanielProject01Iteration1 {
 
 	/**
@@ -14,9 +15,13 @@ public class MadsonDanielProject01Iteration1 {
 		
 		displayCourseGrading();
 		
+		//preparing for enterValidGrade method
+		Scanner input = new Scanner(System.in);
+		enterValidGrade(105, 0, input);
+		
+		
 	}
 	public static void displayCourseGrading() {
-		
 		
 		//display summary of program- grade categories and weights AND grading scale
 		System.out.println("CS1050 Final Grade Calculator\n"
@@ -32,7 +37,7 @@ public class MadsonDanielProject01Iteration1 {
 		final double GUIDED_EXPL_WEIGHT = 0.22;
 		final double MOD_QUIZZES_WEIGHT = 0.22;
 		final double PROJECT_ITER_WEIGHT = 0.22;
-		final double FINAL_WEIGHT = 0.22;
+		final double FINAL_EXAM_WEIGHT = 0.22;
 		
 		
 		//display grade categories and weights
@@ -42,7 +47,7 @@ public class MadsonDanielProject01Iteration1 {
 		System.out.println("Guided Exploration: " + GUIDED_EXPL_WEIGHT*100 + "%");
 		System.out.println("Module Quizzes: " + MOD_QUIZZES_WEIGHT*100 + "%");
 		System.out.println("Project Iterations: " + PROJECT_ITER_WEIGHT*100 + "%");
-		System.out.println("Final: " + FINAL_WEIGHT*100 + "%");
+		System.out.println("Final Exam: " + FINAL_EXAM_WEIGHT*100 + "%");
 		System.out.println("-------------------------------------------------------------------------------");
 		
 		
@@ -54,6 +59,71 @@ public class MadsonDanielProject01Iteration1 {
 		System.out.println("C: 70% to < 80%");
 		System.out.println("D: 60% to < 70%");
 		System.out.println("F: < 60%");
+		System.out.println("-------------------------------------------------------------------------------");
 	}
+	public static double enterValidGrade(double max, double min, Scanner input) {
+		
+		//valid grades are from 0 to 105
+		Scanner gradeInput = new Scanner(System.in);
+		
+		//class participation
+		System.out.println("Enter the grade for the Class Participation.");
+		double gradeEntryCP = gradeInput.nextDouble();
+		
+		while ((gradeEntryCP < 0) || (gradeEntryCP > 105)) {
+			System.out.println("Error: Enter a grade from 0 and 105");
+			gradeEntryCP = gradeInput.nextDouble();
+		}
+		
+		//guided exploration
+		System.out.println("Enter the grade for the Guided Exploration");
+		double gradeEntryGE = gradeInput.nextDouble();
+		
+		while ((gradeEntryGE < 0) || (gradeEntryGE > 105)) {
+			System.out.println("Error: Enter a grade from 0 and 105");
+			gradeEntryGE = gradeInput.nextDouble();
+		}
+		
+		//module quizzes
+		System.out.println("Enter the grade for the Module Quizzes");
+		double gradeEntryMQ = gradeInput.nextDouble();
+		
+		while ((gradeEntryMQ < 0) || (gradeEntryMQ > 105)) {
+			System.out.println("Error: Enter a grade from 0 and 105");
+			gradeEntryMQ = gradeInput.nextDouble();
+		}
 
+		//project iterations
+		System.out.println("Enter the grade for the Project Iterations");
+		double gradeEntryPI = gradeInput.nextDouble();
+
+		while ((gradeEntryPI < 0) || (gradeEntryPI > 105)) {
+			System.out.println("Error: Enter a grade from 0 and 105");
+			gradeEntryPI = gradeInput.nextDouble();
+		}
+		
+		//final exam
+		System.out.println("Enter the grade for the Final Exam");
+		double gradeEntryFE = gradeInput.nextDouble();
+		
+		while ((gradeEntryFE < 0) || (gradeEntryFE > 105)) {
+			System.out.println("Error: Enter a grade from 0 to 105");
+			gradeEntryFE = gradeInput.nextDouble();
+		}
+		gradeInput.close();
+		
+		//declaring all grade % weight constants
+		final double CLASS_PART_WEIGHT = 0.12;
+		final double GUIDED_EXPL_WEIGHT = 0.22;
+		final double MOD_QUIZZES_WEIGHT = 0.22;
+		final double PROJECT_ITER_WEIGHT = 0.22;
+		final double FINAL_EXAM_WEIGHT = 0.22;
+		
+		//TO DO do final percentage calc 
+		double finalGrade = ((gradeEntryCP*CLASS_PART_WEIGHT) + (gradeEntryGE*GUIDED_EXPL_WEIGHT) + 
+				(gradeEntryMQ*MOD_QUIZZES_WEIGHT) + (gradeEntryPI*PROJECT_ITER_WEIGHT) +
+				(gradeEntryFE*FINAL_EXAM_WEIGHT));
+		System.out.println("The final class percentage grade is " + finalGrade + "%");
+		return finalGrade;
+	}
 }
