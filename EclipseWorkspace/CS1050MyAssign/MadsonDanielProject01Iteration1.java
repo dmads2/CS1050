9754390156a9c487bbd8ecfe3bc7d6fef2563da5
dmads2 <dmadson2203@gmail.com>
2024-10-23 21:39:12 -0600

    done project 1

--- EclipseWorkspace/CS1050MyAssign/MadsonDanielProject01Iteration1.java
+++ EclipseWorkspace/CS1050MyAssign/MadsonDanielProject01Iteration1.java
@@ -15,14 +15,29 @@ public class MadsonDanielProject01Iteration1 {
 		
 		displayCourseGrading();
 		
-		//preparing for enterValidGrade method
+		char charRepeat;
 		Scanner input = new Scanner(System.in);
-		//enterValidGrade(105, 0, input);
+		
+		do {
+		//preparing for enterValidGrade method
 		
 		double finalGradePrint = enterValidGrade(105,0, input);
-		System.out.println("The final class percentage grade is " + finalGradePrint + "%");
+		System.out.println("The final class percentage grade is: " + finalGradePrint + "%");
 		
+		char charGradeLetter = determineLetterGrade(finalGradePrint);
+		System.out.println("Final Letter Grade: " + charGradeLetter);
+		
+		System.out.println("Do you wish to calculate another student's grade?"
+				+ "\nEnter y for yes and n for no.");
+		
+		String loopEntry = input.next();
+		charRepeat = loopEntry.charAt(0);
+		//charRepeat = input1.next().charAt(0);
+		} while (charRepeat != 'n');
+		
+		input.close();
 	}
+	
 	public static void displayCourseGrading() {
 		
 		//display summary of program- grade categories and weights AND grading scale
@@ -63,58 +78,58 @@ public class MadsonDanielProject01Iteration1 {
 		System.out.println("F: < 60%");
 		System.out.println("-------------------------------------------------------------------------------");
 	}
+	
 	public static double enterValidGrade(double max, double min, Scanner input) {
 		
 		//valid grades are from 0 to 105
-		Scanner gradeInput = new Scanner(System.in);
 		
 		//class participation
 		System.out.println("Enter the grade for the Class Participation.");
-		double gradeEntryCP = gradeInput.nextDouble();
+		double gradeEntryCP = input.nextDouble();
 		
 		while ((gradeEntryCP < 0) || (gradeEntryCP > 105)) {
 			System.out.println("Error: Enter a grade from 0 and 105");
-			gradeEntryCP = gradeInput.nextDouble();
+			gradeEntryCP = input.nextDouble();
 		}
 		
 		//guided exploration
 		System.out.println("Enter the grade for the Guided Exploration");
-		double gradeEntryGE = gradeInput.nextDouble();
+		double gradeEntryGE = input.nextDouble();
 		
 		while ((gradeEntryGE < 0) || (gradeEntryGE > 105)) {
 			System.out.println("Error: Enter a grade from 0 and 105");
-			gradeEntryGE = gradeInput.nextDouble();
+			gradeEntryGE = input.nextDouble();
 		}
 		
 		//module quizzes
 		System.out.println("Enter the grade for the Module Quizzes");
-		double gradeEntryMQ = gradeInput.nextDouble();
+		double gradeEntryMQ = input.nextDouble();
 		
 		while ((gradeEntryMQ < 0) || (gradeEntryMQ > 105)) {
 			System.out.println("Error: Enter a grade from 0 and 105");
-			gradeEntryMQ = gradeInput.nextDouble();
+			gradeEntryMQ = input.nextDouble();
 		}
 
 		//project iterations
 		System.out.println("Enter the grade for the Project Iterations");
-		double gradeEntryPI = gradeInput.nextDouble();
+		double gradeEntryPI = input.nextDouble();
 
 		while ((gradeEntryPI < 0) || (gradeEntryPI > 105)) {
 			System.out.println("Error: Enter a grade from 0 and 105");
-			gradeEntryPI = gradeInput.nextDouble();
+			gradeEntryPI = input.nextDouble();
 		}
 		
 		//final exam
 		System.out.println("Enter the grade for the Final Exam");
-		double gradeEntryFE = gradeInput.nextDouble();
+		double gradeEntryFE = input.nextDouble();
 		
 		while ((gradeEntryFE < 0) || (gradeEntryFE > 105)) {
 			System.out.println("Error: Enter a grade from 0 to 105");
-			gradeEntryFE = gradeInput.nextDouble();
+			gradeEntryFE = input.nextDouble();
 		}
-		gradeInput.close();
 		
 		//declaring all grade % weight constants
+		//we only need these here to calculate the grade, i would put into its own method but you said not to
 		final double CLASS_PART_WEIGHT = 0.12;
 		final double GUIDED_EXPL_WEIGHT = 0.22;
 		final double MOD_QUIZZES_WEIGHT = 0.22;
@@ -126,6 +141,29 @@ public class MadsonDanielProject01Iteration1 {
 				(gradeEntryFE*FINAL_EXAM_WEIGHT));
 		
 		return finalGrade;
+	}
+	
+	public static char determineLetterGrade(double finalGradePrint) {
+		
+		char charFinalGrade;
+		
+		if (finalGradePrint > 90) {
+			charFinalGrade = 'A';
+		}
+		else if ((finalGradePrint > 80) && (finalGradePrint < 90)) {
+			charFinalGrade = 'B';
+		}
+		else if ((finalGradePrint > 70) && (finalGradePrint < 80)) {
+			charFinalGrade = 'C';
+		}
+		else if ((finalGradePrint > 60) && (finalGradePrint < 70)) {
+			charFinalGrade = 'D';
+		}
+		else {
+			charFinalGrade = 'F';
+		}
+		
+		return charFinalGrade;
 	}
 	
 }
