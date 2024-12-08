f116a58596f52dd5d17edeff6b6b718326c2c60c
dmads2 <dmadson2203@gmail.com>
2024-12-08 00:59:23 -0700

    Create Rizzmas.java

---
+++ EclipseWorkspace/CS1050MyAssign/Rizzmas.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Rizzmas {
+
+	public static void main(String[] args) {
+
+		play();
+		
+		
+		
+	}//end main
+	
+	public static void play() {
+		
+		String[] lyrics = {"A hawk tuah spit on that thang", "two munt buddies", "three Fortnite carts", 
+				"four flickergoons", "five gas station blumpkins", "six skibidi slicers", "seven moldy lunchlys",
+				"eight gyatts-a-clappin", " nine sigmas mogging", "ten booms-a-bringing", "eleven gooners gooning", 
+				"twelve inches busting"};
+		
+		String[] days = {"On the first day of rizzmas my rizzler gave to me",
+				"On the second day of rizzmas my rizzler gave to me",
+				"On the third day of rizzmas my rizzler gave to me",
+				"On the fourth day of rizzmas my rizzler gave to me",
+				"On the fifth day of rizzmas my rizzler gave to me",
+				"On the sixth day of rizzmas my rizzler gave to me",
+				"On the seventh day of rizzmas my rizzler gave to me",
+				"On the eight day of rizzmas my rizzler gave to me",
+				"On the ninth day of rizzmas my rizzler gave to me",
+				"On the tenth day of rizzmas my rizzler gave to me",
+				"On the eleventh day of rizzmas my rizzler gave to me",
+				"On the twelfth day of rizzmas my rizzler gave to me"};
+		
+		for (int i = 0; i < lyrics.length; i++) {
+			System.out.println(days[i] + ": " + lyrics[i]);
+
+		}
+		
+	}
+}//end Rizzmas
